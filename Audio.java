6575fc8d1aca4e6e3c89ee75c38be09c5f4990a2
RyanIp999 <Ryan.Ip25@ycdsbk12.ca>
2024-06-16 23:55:52 -0400

    finished game

--- Audio.java
+++ Audio.java
@@ -6,37 +6,55 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-/**
- * A class that's set up to help play music in other classes
- * @author 
- * Credit goes to my friend Aiden Razack for helping me write this class
- */
-
-// A class that plays music
 public class Audio {
-    Clip clip;
-    AudioInputStream audioInputStream;
+    private Clip clip;
+    private AudioInputStream audioInputStream;
+    private long pausedPosition; // To store the position where playback was paused
+    private boolean isPaused;
 
     public Audio(String filePath) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
         clip = AudioSystem.getClip();
         clip.open(audioInputStream);
+        pausedPosition = 0;
+        isPaused = false;
     }
 
     public void play(int loopCount) {
-        clip.start();
-        if (loopCount == 1) {
-            clip.loop(0);
-        } else if (loopCount == 0) {
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        if (!clip.isRunning()) {
+            if (isPaused) {
+                clip.setMicrosecondPosition(pausedPosition); // Resume from paused position
+                isPaused = false;
+            }
+            clip.start();
+            if (loopCount == 1) {
+                clip.loop(0);
+            } else if (loopCount == 0) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
         }
     }
 
-
     public void stop() {
-        clip.stop();
-        clip.close();
-        System.out.println("Playback stopped.");
+        if (clip.isRunning()) {
+            pausedPosition = clip.getMicrosecondPosition(); // Store current position
+            clip.stop();
+            isPaused = true;
+        }
+    }
+
+    public void reset() {
+        clip.setMicrosecondPosition(0);
+        pausedPosition = 0;
+        isPaused = false;
+    }
+
+    public boolean isRunning() {
+        return clip.isRunning();
+    }
+
+    public void setMicrosecondPosition(long microseconds) {
+        clip.setMicrosecondPosition(microseconds);
     }
 
     public static Audio[] initializeAudioArray(String[] audioPaths) {
@@ -50,13 +68,5 @@ public class Audio {
             }
         }
         return audioArray;
-    }
-    
-    public boolean isRunning() {
-        return clip.isRunning();
-    }
-    
-    public void setMicrosecondPosition(long microseconds) {
-        clip.setMicrosecondPosition(microseconds);
     }
 }
